4d958554232d9534b805f2630023f1a2203e23ab
lameck <lameck.y209@gmail.com>
2022-06-10 16:07:49 +0300

    commit tree 40b298eebd2f3a1c36ded8745cdbdc90c88c6c56

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Lameck';
+const welcomeMessage = 'Welcome to Flatbook, currentUser, Lameck!';
+const excitedWelcomeMessaage = welcomeMessage.toUpperCase();
+ 
